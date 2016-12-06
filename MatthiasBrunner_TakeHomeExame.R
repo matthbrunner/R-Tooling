5a3236c942300ef439fcc8af5cde64b1ac78d346
Matthias Brunner <matthias.brunner@geocom.ch>
2016-12-06 14:03:21 +0100

    Ordnen der Datei

--- MatthiasBrunner_TakeHomeExame.R
+++ MatthiasBrunner_TakeHomeExame.R
@@ -18,8 +18,14 @@ library(foreign)
 # Laden Sie die Daten http://www.farys.org/daten/ebay.dta. Es handelt sich um 
 # Ebaydaten von Mobiltelefonauktionen.
 ebay <- read.dta("http://www.farys.org/daten/ebay.dta")
+
+# Speichern der Daten lokal
+# write.dta(ebay, "C:/Workspace/Weiterbildung/Tooling Und Datenmanagement/TakeHomeExame/Daten/ebay.dta")
+
+# Laden der Lokalen daten
+ebay <- read.dta("C:/Workspace/Weiterbildung/Tooling Und Datenmanagement/TakeHomeExame/Daten/ebay.dta")
 # Betrachten der Daten
 View(ebay)
 
-write.dta(ebay, "C:/Workspace/Weiterbildung/Tooling Und Datenmanagement/TakeHomeExame/Daten/ebay.dta")
+
 
