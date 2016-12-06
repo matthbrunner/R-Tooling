b28a7ebb1ba96adb2c97f53b2e7760a4cb3ba9d8
Matthias Brunner <matthias.brunner@geocom.ch>
2016-12-06 14:02:20 +0100

    Laden der Daten und speichern Lokal

--- MatthiasBrunner_TakeHomeExame.R
+++ MatthiasBrunner_TakeHomeExame.R
@@ -1,13 +1,25 @@
 ### ---
 # Created on 06.12.2016
 # 
+# @author: Matthias Brunner
+# 
 # @summary:
 # 
 # @version: 0.1
 # 
 # @change:
 # 
-# @author: Matthias Brunner
 # 
 # ---
+
+# Hinzufuegen von Library
+library(foreign)
+
+# Laden Sie die Daten http://www.farys.org/daten/ebay.dta. Es handelt sich um 
+# Ebaydaten von Mobiltelefonauktionen.
+ebay <- read.dta("http://www.farys.org/daten/ebay.dta")
+# Betrachten der Daten
+View(ebay)
+
+write.dta(ebay, "C:/Workspace/Weiterbildung/Tooling Und Datenmanagement/TakeHomeExame/Daten/ebay.dta")
 
